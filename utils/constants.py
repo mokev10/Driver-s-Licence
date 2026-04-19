8721c935e68bc65a3f214c7cfdc2465ad0b05668
mokev10 <mokev10@outlook.com>
2026-04-19 17:17:20 +0200

    Create constants.py

---
+++ utils/constants.py
@@ -0,0 +1,19 @@
+IIN_US = {
+    "Alabama": "636033", "Alaska": "636059", "Arizona": "636026", "Arkansas": "636021", "California": "636014",
+    "Colorado": "636020", "Connecticut": "636006", "Delaware": "636011", "Florida": "636010", "Georgia": "636055",
+    "Hawaii": "636047", "Idaho": "636050", "Illinois": "636035", "Indiana": "636037", "Iowa": "636018",
+    "Kansas": "636022", "Kentucky": "636046", "Louisiana": "636007", "Maine": "636041", "Maryland": "636003",
+    "Massachusetts": "636002", "Michigan": "636032", "Minnesota": "636038", "Mississippi": "636053", "Missouri": "636030",
+    "Montana": "636008", "Nebraska": "636028", "Nevada": "636049", "New Hampshire": "636039", "New Jersey": "636036",
+    "New Mexico": "636009", "New York": "636001", "North Carolina": "636004", "North Dakota": "636034", "Ohio": "636023",
+    "Oklahoma": "636058", "Oregon": "636029", "Pennsylvania": "636025", "Rhode Island": "636054", "South Carolina": "636005",
+    "South Dakota": "636042", "Tennessee": "636052", "Texas": "636015", "Utah": "636040", "Vermont": "636024",
+    "Virginia": "636000", "Washington": "636045", "West Virginia": "636057", "Wisconsin": "636031", "Wyoming": "636060",
+    "District of Columbia": "636043"
+}
+
+IIN_CA = {
+    "Alberta": "636048", "British Columbia": "636051", "Manitoba": "636017", "New Brunswick": "636013",
+    "Newfoundland and Labrador": "636027", "Nova Scotia": "636016", "Ontario": "636012",
+    "Prince Edward Island": "636019", "Quebec": "636019", "Saskatchewan": "636044"
+}
