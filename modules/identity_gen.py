0f7127c8d456dab641169e7bb16afb3dee8eef64
mokev10 <mokev10@outlook.com>
2026-04-19 17:29:50 +0200

    Update identity_gen.py

--- modules/identity_gen.py
+++ modules/identity_gen.py
@@ -8,28 +8,36 @@ def show_identity_gen():
     st.header("🪪 Identity Generation Module")
     st.write("Specialized module for generating AAMVA-compliant identity data and PDF417 barcodes.")
     
-    col1, col2 = st.columns([1, 1])
+    st.markdown("### Champs préfixés (saisie)")
+    
+    # Organizing fields into a 2-column grid to match the screenshot
+    col1, col2 = st.columns(2)
     
     with col1:
-        st.subheader("Personal Data")
-        first_name = st.text_input("First Name", "ELIJAH")
-        last_name = st.text_input("Last Name", "WALKER")
-        dob = st.date_input("Date of Birth", datetime.date(1992, 7, 24))
+        dcg = st.text_input("DCG", "CAN", help="Country Identification")
+        dac = st.text_input("DAC", "JEAN", help="First Name")
+        dag = st.text_input("DAG", "1560 SHERBROOKE ST E", help="Street Address")
+        daj = st.selectbox("DAJ", sorted(list(IIN_US.keys()) + list(IIN_CA.keys())), index=55, help="Jurisdiction")
+        dbd = st.text_input("DBD", "20230510", help="Document Issue Date")
+        dbc = st.selectbox("DBC", ["1", "2", "3"], help="Sex (1=Male, 2=Female, 3=Other)")
+        day = st.text_input("DAY", "BRUN", help="Eye Color")
         
     with col2:
-        st.subheader("Regional Config")
-        country = st.selectbox("Country", ["United States", "Canada"])
-        if country == "United States":
-            region = st.selectbox("State", sorted(list(IIN_US.keys())), index=4) # Default to California
-        else:
-            region = st.selectbox("Province", sorted(list(IIN_CA.keys())))
+        dcs = st.text_input("DCS", "NICOLAS", help="Last Name")
+        dbb = st.text_input("DBB", "19941208", help="Date of Birth")
+        dai = st.text_input("DAI", "MONTREAL", help="City")
+        dak = st.text_input("DAK", "H2L4M1", help="Postal Code")
+        dba = st.text_input("DBA", "20310509", help="Document Expiration Date")
+        dau = st.text_input("DAU", "180", help="Height")
+        dcf = st.text_input("DCF", "PEJQ04N96", help="Document Discriminator")
 
     if st.button("Generate AAMVA Data Block & Barcode", use_container_width=True):
-        # Get correct IIN from constants
-        mock_iin = IIN_US.get(region) if country == "United States" else IIN_CA.get(region)
+        # Determine IIN based on DAJ selection
+        mock_iin = IIN_US.get(daj) or IIN_CA.get(daj) or "636000"
         aamva_header = f"ANSI {mock_iin}050102DL00410287ZO02900045DL"
         
-        raw_data = f"@\n{aamva_header}\nDAQD9823415\nDCS{last_name}\nDAC{first_name}\nDBB{dob.strftime('%Y%m%d')}"
+        # Build the data string according to the prefixes
+        raw_data = f"@\n{aamva_header}\nDCG{dcg}\nDCS{dcs}\nDAC{dac}\nDBB{dbb}\nDAG{dag}\nDAI{dai}\nDAJ{daj[:2].upper()}\nDAK{dak}\nDBD{dbd}\nDBA{dba}\nDBC{dbc}\nDAU{dau}\nDAY{day}\nDCF{dcf}"
         
         st.success("AAMVA Data Block Generated Successfully")
         st.code(raw_data, language="text")
@@ -50,7 +58,7 @@ def show_identity_gen():
             st.download_button(
                 label="Download Barcode Image",
                 data=byte_im,
-                file_name=f"dl_barcode_{last_name}.png",
+                file_name=f"dl_barcode_{dcs}.png",
                 mime="image/png",
             )
         except Exception as e:
