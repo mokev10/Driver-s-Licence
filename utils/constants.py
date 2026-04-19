115969828ea1fd51e4485517ef39385da3ecc5f4
mokev10 <mokev10@outlook.com>
2026-04-19 18:49:29 +0200

    Update constants.py

--- utils/constants.py
+++ utils/constants.py
@@ -3,17 +3,19 @@ IIN_US = {
     "Colorado": "636020", "Connecticut": "636006", "Delaware": "636011", "Florida": "636010", "Georgia": "636055",
     "Hawaii": "636047", "Idaho": "636050", "Illinois": "636035", "Indiana": "636037", "Iowa": "636018",
     "Kansas": "636022", "Kentucky": "636046", "Louisiana": "636007", "Maine": "636041", "Maryland": "636003",
-    "Massachusetts": "636002", "Michigan": "636032", "Minnesota": "636038", "Mississippi": "636053", "Missouri": "636030",
-    "Montana": "636008", "Nebraska": "636028", "Nevada": "636049", "New Hampshire": "636039", "New Jersey": "636036",
+    "Massachusetts": "636002", "Michigan": "636032", "Minnesota": "636038", "Mississippi": "636051", "Missouri": "636030",
+    "Montana": "636008", "Nebraska": "636054", "Nevada": "636049", "New Hampshire": "636039", "New Jersey": "636036",
     "New Mexico": "636009", "New York": "636001", "North Carolina": "636004", "North Dakota": "636034", "Ohio": "636023",
-    "Oklahoma": "636058", "Oregon": "636029", "Pennsylvania": "636025", "Rhode Island": "636054", "South Carolina": "636005",
-    "South Dakota": "636042", "Tennessee": "636052", "Texas": "636015", "Utah": "636040", "Vermont": "636024",
-    "Virginia": "636000", "Washington": "636045", "West Virginia": "636057", "Wisconsin": "636031", "Wyoming": "636060",
-    "District of Columbia": "636043"
+    "Oklahoma": "636058", "Oregon": "636029", "Pennsylvania": "636025", "Rhode Island": "636052", "South Carolina": "636005",
+    "South Dakota": "636042", "Tennessee": "636053", "Texas": "636015", "Utah": "636040", "Vermont": "636024",
+    "Virginia": "636000", "Washington": "636045", "West Virginia": "636061", "Wisconsin": "636031", "Wyoming": "636060",
+    "District of Columbia": "636043", "American Samoa": "604427", "Northern Mariana Islands": "604430",
+    "Puerto Rico": "604431", "Guam": "636019", "US State Dept": "636027"
 }
 
 IIN_CA = {
-    "Alberta": "636048", "British Columbia": "636051", "Manitoba": "636017", "New Brunswick": "636013",
-    "Newfoundland and Labrador": "636027", "Nova Scotia": "636016", "Ontario": "636012",
-    "Prince Edward Island": "636019", "Quebec": "636019", "Saskatchewan": "636044"
+    "Alberta": "604432", "British Columbia": "636028", "Manitoba": "636048", "New Brunswick": "636017",
+    "Newfoundland and Labrador": "636016", "Nova Scotia": "636013", "Ontario": "636012",
+    "Prince Edward Island": "604426", "Quebec": "604428", "Saskatchewan": "636044", "Yukon": "604429",
+    "Nunavut": "604433", "Northwest Territories": "604434"
 }
